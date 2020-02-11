62710664490b9df0c52ff4b9b27a635cd8cfd4cd
Kim Oanh <kimoanh2613@gmail.com>
2020-02-11 17:59:20 +0700

    update

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :category
-  max_paginates_per 10
+  max_paginates_per 5
   validates :name, presence: true, length: { maximum: 30 }
   validates :desc, presence: true
+  mount_uploader :image, ImageUploader
 
 end
