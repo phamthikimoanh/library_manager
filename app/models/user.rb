e2d6911bb8fbf1f6e10a9c70f153ea5441a05eb4
Kim Oanh <kimoanh2613@gmail.com>
2020-03-04 16:07:39 +0700

    edit layout book detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,26 @@ class User < ApplicationRecord
     return self.email.split('@')[0].capitalize 
   end
   mount_uploader :image, ImageUploader
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true
+  validates :name, presence: true, length: { minimum: 5 }
+  validate :is_valid_dob?
+
   validates :address, presence: true, length: { minimum: 5 }
   validates :phone, presence: true, length: { minimum: 1, maximun:10 }
-  validate :at_least_15
   max_paginates_per 5
 
-  def at_least_15
-    if self.birthday
-      errors.add(:birthdate, "You must be 15 years or older.") if self.birthday > 15.years.ago.to_date
+  private
+  def is_valid_dob?
+    if((birthday.is_a?(Date) rescue ArgumentError) == ArgumentError)
+      errors.add(:birthday, 'Sorry, Invalid Date of Birth Entered.')
     end
   end
+  # validate :at_least_15
+
+  # def at_least_15
+  #   if self.birthday
+  #     errors.add(:birthdate, "You must be 15 years or older.") if self.birthday > 15.years.ago.to_date
+  #   end
+  # end
 end
