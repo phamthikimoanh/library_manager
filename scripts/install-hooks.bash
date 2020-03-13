b5231edcbd0a66805e5b21475a91056a259eaeb1
Kim Oanh <kimoanh2613@gmail.com>
2020-03-13 10:03:42 +0700

    add file prepare commit msg

--- scripts/install-hooks.bash
+++ scripts/install-hooks.bash
@@ -5,5 +5,6 @@ GIT_DIR=$(git rev-parse --git-dir)
 echo "Installing hooks..."
 # this command creates symlink to our pre-commit script
 ln -s ../../scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
+ln -s ../../scripts/prepare-commit-msg.bash $GIT_DIR/hooks/prepare-commit-msg
 ln -s ../../scripts/pre-push.bash $GIT_DIR/hooks/pre-push
 echo "Done!"
