3379fc5f26f58cdd6610440e78197bec465622ab
Kim Oanh <kimoanh2613@gmail.com>
2020-03-13 10:19:24 +0700

    ...test prepare commit msg 2

--- scripts/prepare-commit-msg.bash
+++ scripts/prepare-commit-msg.bash
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+echo "prepare-commit-msg: Test prepare commit msg";
+
 branchPath = $(git symbolic-ref -q HEAD) #Somthing like refs/heads/myBranchName
 branchName = ${branchPath##*/} #Get text behind the last / of the branch path
 
 firstLine=$(head -n1 $1)
 
 if [ -z "$firstLine"  ] ;then #Check that this is not an amend by checking that the first line is empty
-    echo "prepare-commit-msg: Test prepare commit msg";
+    echo "prepare-commit-msg: Check brand";
     sed -i "1s/^/$branchName: \n/" $1 #Insert branch name at the start of the commit message file
 fi
