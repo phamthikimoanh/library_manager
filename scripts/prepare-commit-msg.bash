a084c9ee85ad930c52e3b94d4c7a3a415b14737a
Kim Oanh <kimoanh2613@gmail.com>
2020-03-13 11:02:34 +0700

    test prepare commit msg ...

--- scripts/prepare-commit-msg.bash
+++ scripts/prepare-commit-msg.bash
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 echo "prepare-commit-msg: Test prepare commit msg 1";
-echo "prepare-commit-msg: Test prepare commit msg 2";
 
-branchPath = $(git symbolic-ref -q HEAD) #Somthing like refs/heads/myBranchName
-branchName = ${branchPath##*/} #Get text behind the last / of the branch path
+COMMIT_MSG_FILE=$1 #$1 first argument
+COMMIT_SOURCE=$2
+SHA1=$3
 
-firstLine=$(head -n1 $1)
+# @PERL_PATH@ -i.bak -ne 'print unless(m/^. Please enter the commit message/..m/^#$/)' "$COMMIT_MSG_FILE"
 
-if [ -z "$firstLine"  ] ;then #Check that this is not an amend by checking that the first line is empty
-    echo "prepare-commit-msg: Check brand";
-    sed -i "1s/^/$branchName: \n/" $1 #Insert branch name at the start of the commit message file
+if [[ -n "$COMMIT_SOURCE" ]];then
+    echo "prepare-commit-msg: Aborting commit due to empty commit message.";
+    # @PERL_PATH@ -i.bak -ne printf 'Please enter the commit message'"$COMMIT_MSG_FILE"
+elif [[ -z "$COMMIT_SOURCE" ]]; then
+  echo "String is not empty, prepare commit msg FINISH"
 fi
