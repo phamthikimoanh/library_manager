2a5c7f70dfe6046167c8d0cddbead790b94172f6
Kim Oanh <kimoanh2613@gmail.com>
2020-03-13 15:07:15 +0700

    test git hook

--- scripts/prepare-commit-msg.bash
+++ scripts/prepare-commit-msg.bash
@@ -4,13 +4,11 @@ echo "prepare-commit-msg: Test prepare commit msg 1";
 
 COMMIT_MSG_FILE=$1 #$1 first argument
 COMMIT_SOURCE=$2
-SHA1=$3
 
-# @PERL_PATH@ -i.bak -ne 'print unless(m/^. Please enter the commit message/..m/^#$/)' "$COMMIT_MSG_FILE"
-
+# -n empty string
 if [[ -n "$COMMIT_SOURCE" ]];then
-    echo "prepare-commit-msg: Aborting commit due to empty commit message.";
-    # @PERL_PATH@ -i.bak -ne printf 'Please enter the commit message'"$COMMIT_MSG_FILE"
-elif [[ -z "$COMMIT_SOURCE" ]]; then
-  echo "String is not empty, prepare commit msg FINISH"
+    echo "prepare-commit-msg: ERROR aborting commit due to empty commit message.";
+# -z not empty string
+else [[ -z "$COMMIT_SOURCE" ]]; then
+  echo "prepare-commit-msg: String is not empty, prepare commit msg FINISH"
 fi
